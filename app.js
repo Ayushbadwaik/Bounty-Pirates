0ba22e8c2e6d8c5b961377a71de8656f5d734f13
Ayushbadwaik <ayushbadwaik.425@gmail.com>
2026-01-16 03:15:26 +0530

    Fix table, images and results

--- app.js
+++ app.js
@@ -2,55 +2,34 @@ import { db } from "./firebase.js";
 import { ref, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const gameRef = ref(db, "game");
-const timerRef = ref(db, "countdown");
 
-let localTimer = 0;
-let interval = null;
+onValue(gameRef, snap => {
 
-window.startGame = function(){
-  intro.classList.add("hidden");
-  main.classList.remove("hidden");
-};
-
-// Countdown
-onValue(timerRef, snap=>{
-  localTimer = parseInt(snap.val());
-  startCountdown();
-});
-
-function startCountdown(){
-  clearInterval(interval);
-  timer.innerText = localTimer;
-  interval = setInterval(()=>{
-    if(localTimer>0){
-      localTimer--;
-      timer.innerText = localTimer;
-    }
-  },1000);
-}
-
-// Game Data
-onValue(gameRef, snap=>{
   const data = snap.val();
 
-  let rows="";
-  data.teams.forEach((t,i)=>{
-    rows+=`
+  // TABLE
+  let rows = "";
+  data.teams.forEach((t,i) => {
+    rows += `
     <tr>
       <td>${t.name}</td>
       <td>${t.players.join(", ")}</td>
-      <td><img src="pirates/p${i+1}.jpg" onerror="this.src='https://i.imgur.com/8Qf7K9N.png'"></td>
-    </tr>
-    `;
+      <td>
+        <img src="pirates/p${i+1}.jpg"
+             onerror="this.src='https://upload.wikimedia.org/wikipedia/commons/5/5a/Pirate_flag.svg'">
+      </td>
+    </tr>`;
   });
+
   document.querySelector("#teamsTable tbody").innerHTML = rows;
 
-  r1.innerText = data.round1.join(", ") || "Not declared";
-  r2.innerText = data.round2.join(", ") || "Not declared";
-  r3.innerText = data.round3.join(", ") || "Not declared";
+  // RESULTS
+  document.getElementById("r1").innerText = data.round1.length ? data.round1.join(", ") : "Not declared";
+  document.getElementById("r2").innerText = data.round2.length ? data.round2.join(", ") : "Not declared";
+  document.getElementById("r3").innerText = data.round3.length ? data.round3.join(", ") : "Not declared";
 
   if(data.winner){
-    winnerBox.innerText = "🏆 Winner: " + data.winner;
-    winSound.play();
+    document.getElementById("winnerBox").innerText = "🏆 Winner: " + data.winner;
   }
+
 });
